32f05577bcf96659d70bf7a1740413c8eecaaf9c
diego.data.icloud@gmail.com <diego.data.icloud@gmail.com>
2022-05-11 15:21:18 -0300

    agregue arrays

--- js/main.js
+++ js/main.js
@@ -1,28 +1,8 @@
-/*function turno(dia, hora) {
-  this.dia = dia;
-  this.hora = hora;
-
-  this.hablar = () => {
-    console.log(
-      "Tu turno queda asi: " +
-        "" +
-        this.dia +
-        "" +
-        ", a las " +
-        "" +
-        this.hora +
-        "" +
-        "hs."
-    );
-  };
-}*/
-
 class Clienta {
-  constructor(nombre, telefono, servicio, abonada) {
+  constructor(nombre, telefono, servicio) {
     this.nombre = nombre;
     this.telefono = telefono;
     this.servicio = servicio;
-    this.abonada = abonada;
   }
 }
 const clientas = [];
@@ -30,12 +10,11 @@ clientas.push(
   new Clienta(
     prompt("Ingresa nombre"),
     prompt("Ingresa telefono: "),
-    "Depilacion",
-    false
+    "Depilacion"
   )
 );
-clientas.push(new Clienta("Maria", "4555 - 1855", "Belleza de pies", false));
-clientas.push(new Clienta("Carolina", "3412 - 4458", "Depilacion", true));
-clientas.push(new Clienta("Elena", "38546 - 1122", "Cosmetologia", false));
-clientas.push(new Clienta("Belen", "4784 - 4584", "Manos", false));
+clientas.push(new Clienta("Maria", "4555 - 1855", "Belleza de pies"));
+clientas.push(new Clienta("Carolina", "3412 - 4458", "Depilacion"));
+clientas.push(new Clienta("Elena", "38546 - 1122", "Cosmetologia"));
+clientas.push(new Clienta("Belen", "4784 - 4584", "Manos"));
 console.log(clientas);
